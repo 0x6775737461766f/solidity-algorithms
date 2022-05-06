d40e86c06f6e6cc2b1c8cc6ecd6595befe828056
Gustavo Oliveira <gt.giacon@gmail.com>
2022-05-06 09:24:01 -0300

    Added dynamic arrays, and all of its related functions

--- algorithms.sol
+++ algorithms.sol
@@ -1,7 +1,40 @@
 pragma solidity ^0.8.0;
 
 contract solAlgorithm {
+
+    uint256[] public _dynamicArr;
     uint256[] public Array = [55, 45, 34 ,43, 10];
+
+    function getArr() public view returns(uint256[] memory) {
+        return _dynamicArr;
+    }
+
+    function getLength() public view returns(uint256) {
+        return _dynamicArr.length;
+    }
+
+    function addElemen(uint256 _number) public returns(uint256[] memory) {
+        _dynamicArr.push(_number);
+        return _dynamicArr;
+    }
+
+    function deleteArr() public returns (uint256[] memory) {
+        uint256 _arrLength = getLength();
+        for (uint i=0; i<_arrLength; i++) {
+            _dynamicArr.pop();
+        }
+        return _dynamicArr;
+    }
+
+    function deleteAtIndex(uint _index) public returns(bool) {
+        _dynamicArr[_index] = _dynamicArr[_dynamicArr.length - 1];
+        _dynamicArr.pop();
+        return true;
+    }
+
+    //SORTING ALGORITHMS
+
+
     function bubbleSort() public pure returns (uint256[] memory) {
         //uint length = Array.length;
         uint[] memory arr = new uint[](4);
