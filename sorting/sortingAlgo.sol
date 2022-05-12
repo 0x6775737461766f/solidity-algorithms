17fda14652462d4a9a2a5517a2de074effb5ad7e
Gustavo Oliveira <gt.giacon@gmail.com>
2022-05-12 10:47:58 -0300

    Modified from contract to library, added binary search

--- sorting/sortingAlgo.sol
+++ sorting/sortingAlgo.sol
@@ -1,11 +1,10 @@
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract sortingAlgo {
-
-    uint256[] Array; //Placeholder
+library sortingAlgo {
 
     //Receives an entire array of elements and only sort them and return the result, not changing the state of the array.
-    function bubbleSort(uint256[] memory _arr) public pure returns (uint256[] memory) {
+    function bubbleSort(uint256[] memory _arr) internal pure returns (uint256[] memory) {
         for(uint i=0; i<_arr.length-1; i++) {
             for(uint j=0; j<_arr.length-1; j++) {
                 if(_arr[j] > _arr[j+1]) {
@@ -18,19 +17,33 @@ contract sortingAlgo {
         return _arr;
     }
 
-    //Bubble sorts an already defined array, changing its state to a sorted one.
-    function bubbleSorter() public returns (uint256[] memory) {
-        uint length = Array.length; //'Array' is a placeholder, change to the array you want to change.
+	//Complete binary search implementation, receives an array, the number to search for, the start index and the end index.
+    function binarySearch(uint256[] memory _arr, uint256 _number, uint low, uint high) internal pure returns(uint256) {
+		 if (high >= low) {
+            uint mid = low + (high - low) / 2;
 
-        for(uint i=0; i<length-1; i++) {
-            for(uint j=0; j<length-1; j++) {
-                if(Array[j] > Array[j+1]) {
-                    uint pivot = Array[j];
-                    Array[j] = Array[j+1];
-                    Array[j+1] = pivot;
-                } 
-            }
+            if (_arr[mid] == _number)
+            return mid;
+
+            if (_arr[mid] > _number)
+            return binarySearch(_arr, _number, low, mid - 1);
+
+            return binarySearch(_arr, _number, mid + 1, high);
         }
-        return Array;
+        return 99999;
+	}
+
+    //Iterative version of binary search, easier to call, only receives the array and the number to look for as parameters.
+    function bSearch(uint256[] memory _arr, uint256 _number) public pure returns(uint256) {
+        uint256 low = 0;
+        uint256 high = _arr.length-1;
+        while (high >= low) {
+            uint256 mid = low + (high - low) / 2;
+
+            if (_arr[mid] == _number) return mid;
+            if (_arr[mid] < _number) low = mid + 1;
+            else high = mid - 1;
+        }
+        return 99999;
     }
 }
